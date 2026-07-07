7aacdc9bfe0081aa73629b15031257e274807799
CaioVMSantos <caiovictorcvm10@gmail.com>
2026-07-07 09:16:19 -0300

    fix: remove hashTest

--- src/test/java/TesteHash.java
+++
@@ -1,9 +0,0 @@
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-public class TesteHash {
-    public static void main(String[] args) {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        System.out.println("Admin: " + encoder.encode("123qwe!@#"));
-        System.out.println("Padrao: " + encoder.encode("123qwe123"));
-    }
-}
